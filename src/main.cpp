0db4a7ea5225ef4cdf04d9fdf51e101b2f82c66e
Rohin Galhotra <rgalhotra@email.arizona.edu>
2018-02-03 10:44:20 -0700

    fixed file parsing?

--- src/main.cpp
+++ src/main.cpp
@@ -8,44 +8,49 @@
 
 int main(int argc, char** argv) {
   int aSize, bSize, aGoal, bGoal, aInt, bInt, stateA, stateB, nextMove, i = 0;
+  std::string temp;
   bool solved = false;
 
 
   if(argc < 3){
-    std:cout << "usage: hw01 input output" << std:endl;
+    std::cout << "usage: hw01 input output" << std::endl;
     exit(EXIT_FAILURE);
   }
 
-  std::strand(std::time(nullptr));
+  std::srand(std::time(nullptr));
   std::ifstream input(argv[1], std::ifstream::in);
   std::ofstream output(argv[2], std::ofstream::out);
 
-  input >> aSize;
+  getline(input, temp, ':');
+  aSize = temp.at(19);
+  getline(input, temp, ':');
   input >> bSize;
+  getline(input, temp, ':');
   input >> aInt;
   input >> bInt;
+  getline(input, temp, ':');
   input >> aGoal;
   input >> bGoal;
   input.close();
   stateA = aInt;
   stateB = bInt;
 
-  output << "Strategy A" << std:endl;
-  output << "Starting out with " << aSize << "-gallon jug and a " << bSize << "-gallon jug" << "              -- state: (" << stateA << "," << stateB << ")" << std:endl;
+  output << "Strategy A" << std::endl;
+  output << "Starting out with " << aSize << "-gallon jug and a " << bSize << "-gallon jug" << "              -- state: (" << stateA << "," << stateB << ")" << std::endl;
   while(i < 251 && !solved){
     nextMove = std::rand() % 6;
     switch(nextMove){
       case 0: //dump A
         if(stateA > 0){
           stateA = 0;
-          output << "Empty the " << aSize << "-gallon jug" << "                                          -- state: (" << stateA << "," << stateB << ")" << std:endl;
+          output << "Empty the " << aSize << "-gallon jug" << "                                          -- state: (" << stateA << "," << stateB << ")" << std::endl;
           i++;
         }
       break;
       case 1: //dump B
         if(stateB > 0){
           stateB = 0;
-          output << "Empty the " << bSize << "-gallon jug" << "                                          -- state: (" << stateA << "," << stateB << ")" << std:endl;
+          output << "Empty the " << bSize << "-gallon jug" << "                                          -- state: (" << stateA << "," << stateB << ")" << std::endl;
           i++;
         }
       break;
@@ -53,7 +58,7 @@ int main(int argc, char** argv) {
         if(stateA > 0 && stateB < bSize){
           stateB = stateA + stateB;
           if(stateB > bSize) stateB = bSize;
-          output << "Pour the " << aSize << "-gallon jug into the " << bSize << "-gallon jug         -- state: (" << stateA << "," << stateB << ")" << std:endl;
+          output << "Pour the " << aSize << "-gallon jug into the " << bSize << "-gallon jug         -- state: (" << stateA << "," << stateB << ")" << std::endl;
           i++;
         }
       break;
@@ -61,21 +66,21 @@ int main(int argc, char** argv) {
         if(stateB > 0 && stateA < aSize){
           stateA = stateA + stateB;
           if(stateA > aSize) stateA = aSize;
-          output << "Pour the " << bSize << "-gallon jug into the " << aSize << "-gallon jug         -- state: (" << stateA << "," << stateB << ")" << std:endl;
+          output << "Pour the " << bSize << "-gallon jug into the " << aSize << "-gallon jug         -- state: (" << stateA << "," << stateB << ")" << std::endl;
           i++;
         }
       break;
       case 4: //fill A
         if(stateA < aSize){
           stateA = aSize;
-          output << "FIll the " << aSize << "-gallon jug                                           -- state: (" << stateA << "," << stateB << ")" << std:endl;
+          output << "FIll the " << aSize << "-gallon jug                                           -- state: (" << stateA << "," << stateB << ")" << std::endl;
           i++;
         }
       break;
       case 5: //fill B
         if(stateB < bSize){
           stateB = bSize;
-          output << "FIll the " << bSize << "-gallon jug                                           -- state: (" << stateA << "," << stateB << ")" << std:endl;
+          output << "FIll the " << bSize << "-gallon jug                                           -- state: (" << stateA << "," << stateB << ")" << std::endl;
           i++;
         }
       break;
@@ -83,14 +88,14 @@ int main(int argc, char** argv) {
         //this shouldn't happen
       break;
     }
-    if(stateA == aGoal && stateB == bGoal) solved = true;
+    if((stateA == aGoal || aGoal == -1) && (stateB == bGoal || bGoal == -1)) solved = true;
   }
 
   //strategy B
   stateA = aInt;
   stateB = bInt;
-  output << std:endl << "Strategy B" << std:endl;
-  output << "Starting out with " << aSize << "-gallon jug and a " << bSize << "-gallon jug" << "              -- state: (" << stateA << "," << stateB << ")" << std:endl;
+  output << std::endl << "Strategy B" << std::endl;
+  output << "Starting out with " << aSize << "-gallon jug and a " << bSize << "-gallon jug" << "              -- state: (" << stateA << "," << stateB << ")" << std::endl;
   //stuff
   output.close();
 }
