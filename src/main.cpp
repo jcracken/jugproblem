510dbd3b16112de13dcb672244a76fc2c515e28a
Rohin Galhotra <rgalhotra@email.arizona.edu>
2018-02-11 10:53:28 -0700

    strat b maybe mostly there?

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,11 @@
 #include <ctime>
 
 int main(int argc, char** argv) {
-  int aSize, bSize, aGoal, bGoal, aInt, bInt, stateA, stateB, nextMove, i = 0;
+  int aSize, bSize, aGoal, bGoal, aInt, bInt, stateA, stateB, nextStateA, nextStateB, nextMove, j, i = 0, loc = 0;
+  std::vector<std::vector<int>> backtrack;
+  std::vector<int> actions;
   std::string temp;
-  bool solved = false;
+  bool solved = false, repeat = false, work = false;
 
 
   if(argc < 3){
@@ -37,7 +39,7 @@ int main(int argc, char** argv) {
 
   output << "Strategy A" << std::endl;
   output << "Starting out with " << aSize << "-gallon jug and a " << bSize << "-gallon jug" << "               -- state: (" << stateA << "," << stateB << ")" << std::endl;
-  while(i < 251 && !solved){
+  while(i < 250 && !solved){
     nextMove = std::rand() % 6;
     switch(nextMove){
       case 0: //dump A
@@ -100,9 +102,118 @@ int main(int argc, char** argv) {
   //strategy B
   stateA = aInt;
   stateB = bInt;
+  i = 0;
   output << std::endl << "Strategy B" << std::endl;
   output << "Starting out with " << aSize << "-gallon jug and a " << bSize << "-gallon jug" << "               -- state: (" << stateA << "," << stateB << ")" << std::endl;
-
-  //stuff
+  while(i < 250 && !solved){
+    nextMove = std::rand() % 6;
+    switch(nextMove){
+      case 0: //dump A
+        if(stateA > 0){
+          nextStateA = 0;
+          i++;
+          work = true;
+        }
+      break;
+      case 1: //dump B
+        if(stateB > 0){
+          nextStateB = 0;
+          i++;
+          work = true;
+        }
+      break;
+      case 2: //pour A into B
+        if(stateA > 0 && stateB < bSize){
+          nextStateB = stateA + stateB;
+          if(nextStateB > bSize){
+            nextStateA = nextStateB - bSize;
+            nextStateB = bSize;
+          }
+          i++;
+          work = true;
+        }
+      break;
+      case 3: //pour B into A
+        if(stateB > 0 && stateA < aSize){
+          nextStateA = stateA + stateB;
+          if(nextStateA > aSize){
+            nextStateB = nextStateA - aSize;
+            nextStateA = aSize;
+          }
+          i++;
+          work = true;
+        }
+      break;
+      case 4: //fill A
+        if(stateA < aSize){
+          nextStateA = aSize;
+          i++;
+          work = true;
+        }
+      break;
+      case 5: //fill B
+        if(stateB < bSize){
+          nextStateB = bSize;
+          i++;
+          work = true;
+        }
+      break;
+      default:
+        //this shouldn't happen
+      break;
+    }
+    if(work){
+      for(j = 0; j < (i - 1); j++){
+        if(backtrack[j][0] == nextStateA && backtrack[j][1] == nextStateB && !repeat){
+          repeat = true;
+          loc = j;
+        }
+      }
+      if(repeat){
+        while(backtrack.size() != (loc + 1)) backtrack.pop_back();
+        while(actions.size() != (loc + 1)) actions.pop_back();
+        i = loc;
+        repeat = false;
+      } else {
+        backtrack.push_back(std::vector<int>());
+        backtrack[i].push_back(nextStateA);
+        backtrack[i].push_back(nextStateB);
+        actions.push_back(nextMove);
+      }
+      stateA = nextStateA;
+      stateB = nextStateB;
+      work = false;
+    }
+    if((stateA == aGoal || aGoal == -1) && (stateB == bGoal || bGoal == -1)) solved = true;
+  }
+  for(j = 0; j < backtrack.size(); j++){
+    stateA = backtrack[j][0];
+    stateB = backtrack[j][1];
+    switch(actions.at(j)){
+      case 0: //dump A
+        output << "Empty the " << aSize << "-gallon jug" << "                                          -- state: (" << stateA << "," << stateB << ")" << std::endl;
+      break;
+      case 1: //dump B
+        output << "Empty the " << bSize << "-gallon jug" << "                                          -- state: (" << stateA << "," << stateB << ")" << std::endl;
+      break;
+      case 2: //pour A into B
+        output << "Pour the " << aSize << "-gallon jug into the " << bSize << "-gallon jug                     -- state: (" << stateA << "," << stateB << ")" << std::endl;
+      break;
+      case 3: //pour B into A
+        output << "Pour the " << bSize << "-gallon jug into the " << aSize << "-gallon jug                     -- state: (" << stateA << "," << stateB << ")" << std::endl;
+      break;
+      case 4: //fill A
+        output << "FIll the " << aSize << "-gallon jug                                           -- state: (" << stateA << "," << stateB << ")" << std::endl;
+      break;
+      case 5: //fill B
+        output << "FIll the " << bSize << "-gallon jug                                           -- state: (" << stateA << "," << stateB << ")" << std::endl;
+      break;
+      default:
+        //this shouldn't happen
+      break;
+    }
+  }
   output.close();
+  backtrack.clear();
+  actions.clear();
 }
